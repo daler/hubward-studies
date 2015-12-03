64fa574bca256df4c788dc94c132e24427f622ac
daler <dalerr@niddk.nih.gov>
2015-12-03 13:09:08 -0500

    improve sexton-2012 metadata and process.py

--- sexton-2012/src/process.py
+++ sexton-2012/src/process.py
@@ -3,11 +3,33 @@ import pandas
 import sys
 import pybedtools
 from pybedtools.contrib import bigbed
-
 infile, outfile = sys.argv[1:3]
+
 df = pandas.read_excel(infile, header=2, index_col=0)
 tmpfile = pybedtools.BedTool._tmp()
-df = df.sort(['chrom', 'start', 'end'])
+df = df.sort_values(['chrom', 'start', 'end'])
 df['epigenetic class'] = df['epigenetic class'].apply(lambda x: x.replace(' ', '_'))
-df.to_csv(tmpfile, header=False, index=False, sep='\t')
+
+colors = {
+    'Active': '153,0,0',
+    'Null': '136,136,136',
+    'HP1_centromeric': '51,102,0',
+    'PcG': '0,51,102',
+}
+df['itemRgb'] = df['epigenetic class'].apply(lambda x: colors[x])
+df['strand'] = '.'
+df['thickStart'] = df['start']
+df['thickEnd'] = df['end']
+df['score'] = 0
+order = [
+    'chrom',
+    'start',
+    'end',
+    'epigenetic class',
+    'score',
+    'strand',
+    'thickStart',
+    'thickEnd',
+    'itemRgb']
+df[order].to_csv(tmpfile, header=False, index=False, sep='\t')
 bigbed.bigbed(pybedtools.BedTool(tmpfile), 'dm3', outfile)
